46b46effd3eb1706e13af62780eed00c74f9ddd1
emmanuelferdinand <emmanuelferdinand42@gmail.com>
2026-02-13 22:51:50 +0800

    Initial commit

---
+++ guess-the-word/src/main/java/com/example/guess_the_word/GuessTheWordApplication.java
@@ -0,0 +1,13 @@
+package com.example.guess_the_word;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuessTheWordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuessTheWordApplication.class, args);
+	}
+
+}
